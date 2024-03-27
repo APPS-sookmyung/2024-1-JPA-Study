e401827696a77a07834c03b48140c32a9d45fce9
chan_nie <126068623+chennielee@users.noreply.github.com>
2024-03-27 13:50:36 +0900

    mod: week1 typo

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,31 +1,33 @@
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-//import javax.persistence.EntityManager;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
     @Test
     @Transactional
-    @Rollback(false)
+    @Rollback(value = false)
     public void testMember() {
+        // given
         Member member = new Member();
         member.setUsername("memberA");
+
+        // when
         Long savedId = memberRepository.save(member);
         Member findMember = memberRepository.find(savedId);
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+
+        // then
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+        Assertions.assertEquals(findMember, member); // JPA 엔티티 동일성 보장
     }
 }

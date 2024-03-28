924e8e7aca41caf138b2b418eff913a2ebc20391
이해림 <lhaerim1229@naver.com>
2024-03-28 11:54:49 +0900

    Style: 1주차 과제 build.gradle 문법 오류 수정

--- 1주차/test/java/jpabook.jpashop/MemberRepositoryTest.java
+++ 1주차/test/java/jpabook.jpashop/MemberRepositoryTest.java
@@ -4,20 +4,23 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @Transactional

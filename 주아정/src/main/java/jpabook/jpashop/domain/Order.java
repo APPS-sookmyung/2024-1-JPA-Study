a114b473f1a45b1e4fc539e2171f74e49df583ea
ajeong <ajung7038@naver.com>
2024-04-27 14:12:11 +0900

    Feat: 연관관계 메서드 추가

--- 주아정/src/main/java/jpabook/jpashop/domain/Order.java
+++ 주아정/src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,4 +33,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
+
+    // == 연관관계 메서드 == //
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }

4b0562c7de3ad8cd282e25eb148080c7a6334dd5
ljinny <leejieun2003@gmail.com>
2024-05-01 01:51:44 +0900

    Feat: 3주차 실습 코드

--- 이지은/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 이지은/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -18,4 +22,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }

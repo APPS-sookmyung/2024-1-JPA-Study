f5827a5d77d5fa8891fc056fa2a630141fa67a13
chan_nie <126068623+chennielee@users.noreply.github.com>
2024-05-14 17:09:38 +0900

    feat: 5,6주차 실습코드

--- 이채은/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 이채은/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain.item;
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
 import jpabook.jpashop.domain.CategoryItem;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,18 +12,30 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
 
-    @Id @GeneratedValue
-    @Column(name = "item_id")
-    private Long id;
-    private String name;
-    private int price;
-    private int stockQuantity;
+    public abstract class Item {
+        @Id
+        @GeneratedValue
+        @Column(name = "item_id")
+        private Long id;
+        private String name;
+        private int price;
+        private int stockQuantity;
+        @ManyToMany(mappedBy = "items")
+        private List<Category> categories = new ArrayList<Category>();
 
-//    @ManyToMany(mappedBy = "items")
-//    private List<Category> categories = new ArrayList<>();
-    @OneToMany(mappedBy = "item", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<CategoryItem> categoryItems = new ArrayList<>();
+        //==비즈니스 로직==//
+        public void addStock(int quantity) {
+            this.stockQuantity += quantity;
+        }
 
-}
+        public void removeStock(int quantity) {
+            int restStock = this.stockQuantity - quantity;
+            if (restStock < 0) {
+                throw new NotEnoughStockException("need more stock");
+            }
+            this.stockQuantity = restStock;
+        }
+    }

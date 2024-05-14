75ee889e8e22c479038c2087a890631efc950033
chan_nie <126068623+chennielee@users.noreply.github.com>
2024-05-14 13:34:13 +0900

    feat: making memberService && test case

--- 이채은/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ 이채은/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -4,16 +4,26 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public class MemberRepository {
     @PersistenceContext
     EntityManager em;
-    public Long save(Member member) {
+    public void save(Member member) {
         em.persist(member);
-        return member.getId();
     }
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name",
+                        Member.class)
+                .setParameter("name", name)
+                .getResultList();
     }
 }
